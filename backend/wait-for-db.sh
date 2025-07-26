2a0db2d38b2ebcfe74b2ec7ff5af6bb78c3301e5
hadasdavid8 <hadasdavid8@gmail.com>
2025-07-26 19:30:29 +0300

    Remove db service and update wait-for-db.sh

--- backend/wait-for-db.sh
+++ backend/wait-for-db.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
-
 echo "Waiting for MySQL to be ready..."
 
-while ! nc -z db 3306; do
+while ! nc -z viagemi-db.c9yik8gk2zvq.eu-north-1.rds.amazonaws.com 3306; do
   echo "Waiting for database connection..."
   sleep 1
 done
