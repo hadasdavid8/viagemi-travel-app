dd3436d462b40b0a193643deb6b3b5679abe2a0e
hadasdavid8 <hadasdavid8@gmail.com>
2025-10-22 16:34:16 +0300

    Change startup.sh file

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,20 +1,30 @@
-echo "Running Frontend Install & Build..."
+set -e 
+
+SCRIPT_DIR=$(dirname "$0")
+cd "$SCRIPT_DIR"
+
+echo "--- Running Frontend Install & Build ---"
+
 cd frontend
+
 npm install 
 npm run build 
+
 cd .. 
 
-echo "Navigating to backend directory (for manage.py)..."
+echo "--- Running Django Commands ---"
+
 cd backend
 
 echo "Running Migrations..."
-python manage.py migrate
+python manage.py migrate --noinput
 
 echo "Collecting Static Files..."
-
 python manage.py collectstatic --noinput
 
+cd ..
 
-echo "Starting Gunicorn..."
-
+echo "--- Starting Gunicorn Server ---"
+cd backend
 gunicorn planner.wsgi --bind 0.0.0.0:8000
+
